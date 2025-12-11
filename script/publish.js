6b2b4f0eaa25541dd854cd4694e12f52bab8e9f9
zii-han <farhan45craftofficial@gmail.com>
2025-12-11 17:32:44 +0700

    Update publish.js

--- script/publish.js
+++ script/publish.js
@@ -1,9 +1,9 @@
- //kirim pesan
-    document.getElementById("kirim").onclick = () => {
+     //kirim pesan
+    document.getElementById("kirim").onclick = cirim();
+    function cirim() {
       const text = document.getElementById("pesan").value;
       if (!text) return alert("Isi pesan dulu!");
 
       client.publish("mymqttforproject", text);
       console.log("Dikirim:", text);
-
-    };
+    }
