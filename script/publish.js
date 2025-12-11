8b6fc8b468f490489c6d4930ee75e6f56f4594d6
zii-han <farhan45craftofficial@gmail.com>
2025-12-11 21:52:46 +0700

    Refactor message sending to 'sent' function
    Refactor message sending logic into a separate function.

--- script/publish.js
+++ script/publish.js
@@ -1,24 +1,8 @@
-const client = mqtt.connect("wss://broker.emqx.io:8084/mqtt");
-
-    client.on("connect", () => {
-      document.getElementById("status").innerText = "Status: Terkoneksi ke MQTT";
-      console.log("MQTT Web Connected");
-    });
-
-    client.on("reconnect", () => {
-      document.getElementById("status").innerText = "Status: Reconnecting...";
-    });
-
-    client.on("error", (err) => {
-      document.getElementById("status").innerText = "Error MQTT: " + err;
-    });
-
     // KIRIM PESAN
-    document.getElementById("kirim").onclick = () => {
+    function sent() {
       const text = document.getElementById("pesan").value;
       if (!text) return alert("Isi pesan dulu!");
 
       client.publish("mymqttforproject", text);
       console.log("Dikirim:", text);
-    };
-
+    }
