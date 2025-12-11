3430f08b12a07b99fe0a0769bd24f4368a175216
zii-han <farhan45craftofficial@gmail.com>
2025-12-11 17:37:30 +0700

    Update publish.js

--- script/publish.js
+++ script/publish.js
@@ -1,9 +1,23 @@
-     //kirim pesan
-    document.getElementById("kirim").onclick = cirim();
-    function cirim() {
+const client = mqtt.connect("wss://broker.emqx.io:8084/mqtt");
+
+    client.on("connect", () => {
+      document.getElementById("status").innerText = "Status: Terkoneksi ke MQTT";
+      console.log("MQTT Web Connected");
+    });
+
+    client.on("reconnect", () => {
+      document.getElementById("status").innerText = "Status: Reconnecting...";
+    });
+
+    client.on("error", (err) => {
+      document.getElementById("status").innerText = "Error MQTT: " + err;
+    });
+
+    // KIRIM PESAN
+    document.getElementById("kirim").onclick = () => {
       const text = document.getElementById("pesan").value;
       if (!text) return alert("Isi pesan dulu!");
 
-      client.publish("mymqttforproject", text);
+      client.publish("abdianjaymabar", text);
       console.log("Dikirim:", text);
-    }
+    };
